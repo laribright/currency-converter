03f151af872b15fa23b222d1c342cb4552fdf78e
laribright <laribright8@gmail.com>
2022-11-10 00:30:32 +0100

    render the <Header /> component in the <App /> component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./styles/main.scss";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div className="">HELLO WORLD</div>;
+  return (
+    <div className="">
+      <Header />
+      HELLO WORLD
+    </div>
+  );
 }
 
 export default App;
