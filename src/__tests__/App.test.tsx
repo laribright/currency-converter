70d75f17efe96f5ece4596ae7eaba19d95c63aef
laribright <laribright8@gmail.com>
2022-11-10 12:12:18 +0100

    Wrap the App test file with the Browser Wrapper component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "../App";
 
 describe("<App />", () => {
   it("renders the heading component", async () => {
-    render(<App />);
+    render(<App />, {wrapper: BrowserRouter});
 
     const header = await screen.findByRole("navigation");
 
