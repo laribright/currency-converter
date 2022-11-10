498b4ea4e998e62487ba1365e85041f055d4be05
laribright <laribright8@gmail.com>
2022-11-10 12:13:56 +0100

    Wrap app component with Fragment tag instead of div

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="">
+    <>
       <Header />
       HELLO WORLD
-    </div>
+    </>
   );
 }
 
