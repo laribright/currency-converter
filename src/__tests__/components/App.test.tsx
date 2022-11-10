9b0d6385dfa2139803786cfe152183d45326de3b
laribright <laribright8@gmail.com>
2022-11-10 14:04:27 +0100

    Refactor tests directory

--- src/__tests__/components/App.test.tsx
+++ src/__tests__/components/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "../App";
+import App from "../../App";
 
 describe("<App />", () => {
   it("renders the heading component", async () => {
--- src/__tests__/components/Header.test.tsx
+++ src/__tests__/components/Header.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
 
-import Header from "../components/Header/Header";
+import Header from "../../components/Header/Header";
 
 describe("<Header />", () => {
   const setup = () => render(<Header />, { wrapper: BrowserRouter });
