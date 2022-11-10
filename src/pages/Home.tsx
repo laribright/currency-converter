f89ca904c56e1e8c59efd7e95a6099de515ba8b5
laribright <laribright8@gmail.com>
2022-11-10 23:57:21 +0100

    Render a select dropdown with default value of EUR and also a label text of From

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,6 +6,13 @@ const Home = () => {
         <input type="number" id="Amount" />
       </div>
 
+      <div>
+        <label htmlFor="from">From</label>
+        <select data-testid="from" id="from" value={"EUR"}>
+          <option value="EUR">EUR</option>
+        </select>
+      </div>
+
       <button type="button">Swap</button>
       <button type="submit">Convert</button>
     </div>
