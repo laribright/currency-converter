65a41a961a6120758d3cd2d4568f7f59e9e1ab02
laribright <laribright8@gmail.com>
2022-11-10 17:13:17 +0100

    Render the Home Page in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
 import "./styles/main.scss";
 import Header from "./components/Header/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <Header />
-      HELLO WORLD
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
