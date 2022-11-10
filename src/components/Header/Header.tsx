bd8fe12b78bfc3a47dd536d906f150f4cfe38438
laribright <laribright8@gmail.com>
2022-11-10 12:13:19 +0100

    Add the Navigation Links (EUR-USD Details, EUR-GBP Details) to header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
   return (
     <header role="navigation">
       <h2>Logo</h2>
+
+      <Link to="/eur-usd">EUR-USD Details</Link>
+      <Link to="/eur-gbp">EUR-GBP Details</Link>
     </header>
   );
 };
