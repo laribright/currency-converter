6c2e3b5e541d2ab5f2bd3b9cf222ca5117fac348
laribright <laribright8@gmail.com>
2022-11-11 20:38:12 +0100

    Remove all imports from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,3 @@
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
-
-import {
-  convertCurrencies,
-  fetchCurrencies,
-  onSwapClicked,
-  updateConversionAmount,
-  updateCurrencyFrom,
-  updateCurrencyTo,
-} from "../features/currency";
-import { useAppDispatch, useAppSelector } from "../hooks/storeHooks";
-
 const Home = () => {
   return <div></div>;
 };
