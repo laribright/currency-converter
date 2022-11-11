cc6363fa432a130bc4446cbe80b5d99fd33181d4
laribright <laribright8@gmail.com>
2022-11-11 19:38:23 +0100

    Adding action to convert currencies

--- src/features/currency.ts
+++ src/features/currency.ts
@@ -1,16 +1,58 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
+
+import { API_URL } from "./../constants/api";
+
+interface IConvertCurrenciesFn {
+  from: string;
+  to: string;
+  amount: number;
+}
+
+// ASYNC ACTION
+export const convertCurrencies = createAsyncThunk(
+  "currencies/convertCurrencies",
+  async (args: IConvertCurrenciesFn, thunkApi) => {
+    const { from, to, amount } = args;
+
+    const config = {
+      method: "GET",
+      headers: { apikey: process.env.REACT_APP_API_LAYER_KEY! },
+    };
+
+    try {
+      const response = await fetch(
+        `${API_URL}/convert?to=${to}&from=${from}&amount=${amount}`,
+        config
+      );
+      const data = await response.json();
+      return data.result;
+    } catch (error: any) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
 
 export interface ICurrencyState {
   currencyFrom: string;
   currencyTo: string;
   conversionAmount: number;
+  convertedData: {
+    amount: null | number;
+    loading: boolean;
+    error: null | string;
+  };
 }
 
 const initialState: ICurrencyState = {
   currencyFrom: "EUR",
   currencyTo: "USD",
   conversionAmount: 1,
+  convertedData: {
+    amount: null,
+    error: null,
+    loading: false,
+  },
 };
 
 export const currencySlice = createSlice({
@@ -27,6 +69,25 @@ export const currencySlice = createSlice({
         currencyTo: state.currencyFrom,
       };
     },
+  },
+  extraReducers(builder) {
+    builder.addCase(convertCurrencies.pending, (state) => {
+      state.convertedData.loading = true;
+    });
+    builder.addCase(
+      convertCurrencies.fulfilled,
+      (state, action: PayloadAction<number>) => {
+        state.convertedData.loading = false;
+        state.convertedData.amount = action.payload;
+      }
+    );
+    builder.addCase(
+      convertCurrencies.rejected,
+      (state, action: PayloadAction<any>) => {
+        state.convertedData.loading = false;
+        state.convertedData.error = action.payload;
+      }
+    );
   },
 });
 
