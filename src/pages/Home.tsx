16009320c8981f1cc7000ba18d0607dd3a844d57
laribright <laribright8@gmail.com>
2022-11-11 08:24:57 +0100

    Write convert currencies function in utils directory

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,51 @@
+import { useEffect, useState } from "react";
+
+import { convertCurrencies } from "../utils/api";
+
 const Home = () => {
+  const [currencyFrom, setCurrencyFrom] = useState("EUR");
+  const [currencyTo, setCurrencyTo] = useState("USD");
+  const [conversionAmount, setConversionAmount] = useState(1);
+  const [convertedResult, setConvertedResult] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const args = {
+          amount: conversionAmount,
+          from: currencyFrom,
+          to: currencyTo,
+        };
+        const result = await convertCurrencies(args);
+        setConvertedResult(result.result);
+      } catch (error) {
+        alert("Can't fetch Data");
+      }
+    })();
+  }, [conversionAmount, currencyFrom, currencyTo]);
+
   return (
     <div data-testid="home-page">
       <div>
         <label htmlFor="Amount">Amount</label>
-        <input type="number" id="Amount" />
+        <input type="number" id="Amount" value={conversionAmount} />
       </div>
 
       <div>
         <label htmlFor="from">From</label>
-        <select data-testid="from" id="from" value={"EUR"}>
-          <option value="EUR">EUR</option>
+        <select data-testid="from" id="from" value={currencyFrom}>
+          <option value={currencyFrom}>{currencyFrom}</option>
         </select>
       </div>
-      
+
       <div>
         <label htmlFor="to">To</label>
-        <select data-testid="to" id="to" value={"USD"}>
-          <option value="USD">USD</option>
+        <select data-testid="to" id="to" value={currencyTo}>
+          <option value={currencyTo}>{currencyTo}</option>
         </select>
       </div>
+
+      <div data-testid="conversion-result">{convertedResult}</div>
 
       <button type="button">Swap</button>
       <button type="submit">Convert</button>
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,36 @@
+import { API_URL } from "./../constants/api";
+
 export const fetchCurrencies = async () => {
   try {
     const response = await fetch(
-      `https://data.fixer.io/api/symbols?access_key=${process.env.REACT_APP_API_LAYER_KEY}`
+      `${API_URL}/symbols?access_key=${process.env.REACT_APP_API_LAYER_KEY}`
     );
-    return await response.json();
+    return response.json();
+  } catch (error: any) {
+    throw new Error(error);
+  }
+};
+
+interface IConvertCurrenciesFn {
+  from: string;
+  to: string;
+  amount: number;
+}
+
+export const convertCurrencies = async (args: IConvertCurrenciesFn) => {
+  const { from, to, amount } = args;
+
+  const config = {
+    method: "GET",
+    headers: { apikey: process.env.REACT_APP_API_LAYER_KEY! },
+  };
+
+  try {
+    const response = await fetch(
+      `${API_URL}/convert?to=${to}&from=${from}&amount=${amount}`,
+      config
+    );
+    return response.json();
   } catch (error: any) {
     throw new Error(error);
   }
