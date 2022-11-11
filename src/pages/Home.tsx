eaaf10ffb56d3319b32a7d7dd4534dc21054d927
laribright <laribright8@gmail.com>
2022-11-11 09:40:49 +0100

    Add stying for Home page components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,49 +6,93 @@ const Home = () => {
   const [currencyFrom, setCurrencyFrom] = useState("EUR");
   const [currencyTo, setCurrencyTo] = useState("USD");
   const [conversionAmount, setConversionAmount] = useState(1);
-  const [convertedResult, setConvertedResult] = useState("");
+  const [convertedResult, setConvertedResult] = useState("1.2");
 
   useEffect(() => {
-    (async () => {
-      try {
-        const args = {
-          amount: conversionAmount,
-          from: currencyFrom,
-          to: currencyTo,
-        };
-        const result = await convertCurrencies(args);
-        setConvertedResult(result.result);
-      } catch (error) {
-        alert("Can't fetch Data");
-      }
-    })();
+    // (async () => {
+    //   try {
+    //     const args = {
+    //       amount: conversionAmount,
+    //       from: currencyFrom,
+    //       to: currencyTo,
+    //     };
+    //     const result = await convertCurrencies(args);
+    //     setConvertedResult(result.result);
+    //   } catch (error) {
+    //     alert("Can't fetch Data");
+    //   }
+    // })();
   }, [conversionAmount, currencyFrom, currencyTo]);
 
   return (
-    <div data-testid="home-page">
-      <div>
-        <label htmlFor="Amount">Amount</label>
-        <input type="number" id="Amount" value={conversionAmount} />
+    <div className="home-page" data-testid="home-page">
+      <div className="currency-box--controls">
+        <div>
+          <div className="form-group">
+            <label className="form-group--label" htmlFor="Amount">
+              Amount
+            </label>
+            <input
+              className="input"
+              type="number"
+              id="Amount"
+              value={conversionAmount}
+            />
+          </div>
+
+          <div className="btn">1 EUR = 1.2 USD</div>
+        </div>
+
+        <div className="conversion-controls">
+          <div className="conversion-controls__select">
+            <div className="form-group">
+              <label className="form-group--label" htmlFor="from">
+                From
+              </label>
+              <select
+                className="form-group--select"
+                data-testid="from"
+                id="from"
+                value={currencyFrom}
+              >
+                <option value={currencyFrom}>{currencyFrom}</option>
+              </select>
+            </div>
+
+            <button type="button" className="btn">
+              Swap
+            </button>
+
+            <div className="form-group">
+              <label className="form-group--label" htmlFor="to">
+                To
+              </label>
+              <select
+                className="form-group--select"
+                data-testid="to"
+                id="to"
+                value={currencyTo}
+              >
+                <option value={currencyTo}>{currencyTo}</option>
+              </select>
+            </div>
+          </div>
+
+          <button type="submit" className="btn">
+            Convert
+          </button>
+
+          <div className="conversion-result--box">
+            <div data-testid="conversion-result" className="conversion-result">
+              {convertedResult} {currencyTo}
+            </div>
+
+            <a className="conversion-details--link" href="#!">
+              More Details
+            </a>
+          </div>
+        </div>
       </div>
-
-      <div>
-        <label htmlFor="from">From</label>
-        <select data-testid="from" id="from" value={currencyFrom}>
-          <option value={currencyFrom}>{currencyFrom}</option>
-        </select>
-      </div>
-
-      <div>
-        <label htmlFor="to">To</label>
-        <select data-testid="to" id="to" value={currencyTo}>
-          <option value={currencyTo}>{currencyTo}</option>
-        </select>
-      </div>
-
-      <div data-testid="conversion-result">{convertedResult}</div>
-
-      <button type="button">Swap</button>
-      <button type="submit">Convert</button>
     </div>
   );
 };
