117c647b71b956680a97dcdc3cba7eba8cbdd909
laribright <laribright8@gmail.com>
2022-11-12 07:43:23 +0100

    Populate home page with conversion result grid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,30 @@
+import { useAppSelector } from "../hooks/storeHooks";
+
 const Home = () => {
-  return <div data-testid="home-page">HOME PAGE</div>;
+  const CONVERSION_RESULT = new Array(9).fill(2);
+
+  const {
+    currency: {
+      convertedData: { amount },
+      currencyFrom,
+      currencyTo,
+      conversionAmount,
+    },
+  } = useAppSelector((state) => state);
+
+  return (
+    <div className="home-page" data-testid="home-page">
+      <div className="grid-display">
+        {CONVERSION_RESULT.map((result, idx) => {
+          return (
+            <div key={idx} className="converted-result-card">
+              {conversionAmount} {currencyFrom} = {amount} {currencyTo}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
