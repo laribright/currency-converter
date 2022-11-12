6701fb193383f3ed052941e824f48b5283b60617
laribright <laribright8@gmail.com>
2022-11-12 06:04:17 +0100

    Add navigation header links to currency details page. Dispatch updateCurrencyFrom and updateCurrencyTo before navigating

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
+import { updateCurrencyFrom, updateCurrencyTo } from "../../features/currency";
+import { useAppDispatch } from "../../hooks/storeHooks";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const onNavigate = (param: string) => {
+    const currencyFrom = param.split("-")[0].toUpperCase();
+    const currencyTo = param.split("-")[1].toUpperCase();
+
+    dispatch(updateCurrencyFrom(currencyFrom));
+    dispatch(updateCurrencyTo(currencyTo));
+
+    navigate("/currency/details");
+  };
+
   return (
     <header className="header" data-testid="header">
       <h2 className="header__logo">Logo</h2>
 
       <div className="header__navigation-box">
-        <Link to="/eur-usd" className="header__navigation-link">
+        <button
+          onClick={() => onNavigate("eur-usd")}
+          className="header__navigation-link"
+        >
           EUR-USD Details
-        </Link>
-        <Link to="/eur-gbp" className="header__navigation-link">
+        </button>
+        <button
+          onClick={() => onNavigate("eur-gbp")}
+          className="header__navigation-link"
+        >
           EUR-GBP Details
-        </Link>
+        </button>
       </div>
     </header>
   );
